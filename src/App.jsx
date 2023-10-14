17f91ba7fc48f75f8d47a1931ae2a87a37f15766
pearlescence-m <meruyert.jakipbekova@gmail.com>
2023-10-14 15:25:28 -0400

    initialized with passage auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import { PassageProvider } from "@passageidentity/passage-react";
+import { PassageProvider } from '@passageidentity/passage-react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./views/Home";
+import Dashboard from "./views/Dashboard";
+import Profile from "./views/Profile";
+import Banner from "./components/banner";
+import styles from "./styles/App.module.css";
+
 // import '@passageidentity/passage-elements/passage-auth'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    {/* <PassageProvider appId={process.env.REACT_APP_PASSAGE_APP_ID}> */}
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Welcome to the dance break</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      {/* </PassageProvider> */}
+      <PassageProvider appId={process.env.REACT_APP_PASSAGE_APP_ID}>
+        <div>
+          <Banner />
+          <div className={styles.mainContainer}>
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/dashboard" element={<Dashboard />}></Route>
+              <Route path="/profile" element={<Profile />}></Route>
+            </Routes>
+          </div>
+        </div>
+      </PassageProvider>
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,33 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        icons: [
-          {
-            src: '/icons/icon512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ],
-      },
-      
-      devOptions: {
-        enabled: true,
-      },
-    }),
-  ],
-});
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    define: {
+      'process.env.REACT_APP_PASSAGE_APP_ID': JSON.stringify(env.REACT_APP_PASSAGE_APP_ID)
+    },
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        manifest: {
+          icons: [
+            {
+              src: '/icons/icon512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable'
+            }
+          ],
+        },
+        
+        devOptions: {
+          enabled: true,
+        },
+      }),
+    ],
+}});
