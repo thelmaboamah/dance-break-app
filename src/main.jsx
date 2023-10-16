941d3574f9c5325505714f242831647ff64d8dc7
Thelma Boamah <thelma.a.boamah@gmail.com>
2023-10-16 07:48:53 -0400

    testing subscribing to database inserts in a simple todo app

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import App2 from "./App2.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <App2 />
     </Router>
   </React.StrictMode>,
 );
