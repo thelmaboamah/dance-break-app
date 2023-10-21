a0abb471b2ed81cdc1faa08fef8e4b08667f3ffb
Thelma Boamah <thelma.a.boamah@gmail.com>
2023-10-21 00:44:50 -0400

    update icons and manifest

--- vite.config.js
+++ vite.config.js
@@ -10,12 +10,19 @@ export default defineConfig(() => {
       VitePWA({
         registerType: "autoUpdate",
         manifest: {
+          name: "Dance Break",
+          short_name: "Dance Break",
+          description: "Improve your health by moving to your favorite songs",
           icons: [
             {
-              src: "/icons/icon512.png",
+              src: "/icons/logo.png",
               sizes: "512x512",
               type: "image/png",
               purpose: "any maskable",
+            },
+            {
+              src: "/icons/dance_break_logo.svg",
+              sizes: "72x72 96x96 128x128 256x256 512x512 1024x1024",
             },
           ],
         },
