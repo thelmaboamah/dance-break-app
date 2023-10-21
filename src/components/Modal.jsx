d14da558a04aeeb61f4c82ed469b9375365c9b7c
Sylvia Boamah <sbsboamah@gmail.com>
2023-10-21 18:53:01 -0400

    add download and log out

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,49 @@
 import Chevron from "../../public/icons/chevron-right-solid.svg";
-
+import { useEffect, useRef } from "react";
+import { usePassageLogout } from "../hooks";
+import { useNavigate } from "react-router-dom";
 export default function Modal() {
+  const { logout } = usePassageLogout();
+
+  const navigate = useNavigate();
+
+  const signout = () => {
+    logout();
+    navigate("/");
+  };
+
+  const bipEvent = useRef(null);
+
+  function installListener(event) {
+    event.preventDefault();
+    console.log("Inside install listener");
+    bipEvent.current = event;
+  }
+
+  function handleInstallClick() {
+    if (bipEvent.current) {
+      bipEvent.current.prompt();
+    } else {
+      // incompatible browser, your PWA is not passing the criteria, the user has already installed the PWA
+      //TODO: show the user information on how to install the app
+      alert(
+        "To install the app look for Add to Homescreen or Install in your browser's menu",
+      );
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", installListener);
+
+    return () => {
+      window.removeEventListener("beforeinstallprompt", installListener);
+    };
+  }, []);
+
   return (
-    // <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 h-screen ">
     <div className="bg-white fixed right-0 top-0 h-screen desktop:w-1/4 w-[238px]">
       <div className="bg-white rounded-lg shadow-lg w-96 h-screen">
         <div className="p-6">
-          {/* closign icon */}
-          {/* <div className="flex justify-end mr-8">
-            <img src={XSolid} alt="X-Solid" className="w-5 h-5 "/>
-          </div> */}
           <div className="bg-gray-100 rounded-t-lg px-6 py-4"></div>
 
           {/* 5 rows */}
@@ -27,13 +61,13 @@ export default function Modal() {
               </button>
             </li>
             <li>
-              <button className="hover:underline focus:outline-none">
+              <button onClick={handleInstallClick} className="hover:underline focus:outline-none">
                 Download the app
               </button>
             </li>
             <li><div className="bg-grey border-t-1 desktop:w-3/4 w-[189px] h-[1px]"></div></li>
             <li>
-              <button className="hover:underline focus:outline-none">
+              <button onClick={signout} className="hover:underline focus:outline-none">
                 Log out
               </button>
             </li>
