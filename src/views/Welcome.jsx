d459a1964bc00a8be873ee53911440850c0f1f57
Sylvia Boamah <sbsboamah@gmail.com>
2023-10-21 00:28:57 -0400

    run prettier

--- src/views/Welcome.jsx
+++ src/views/Welcome.jsx
@@ -20,28 +20,33 @@ function Welcome() {
   return (
     <PassageAuthGuard unAuthComp={<AuthRedirect />}>
       <>
-      <div className="desktop:flex-1">
-        <img
-          className="w-full desktop:block hidden h-screen object-cover"
-          src="public/images/girl-dancing.jpg"
-          alt="Girl dancing in front of a yellow background"
-        ></img>
-      </div>
-      <section className="desktop:flex-1 px-64">
-        <LogoutButton /> 
-        {/* leaving logout here since we dont have menu yet*/}
-        <h1 className="font-dmSans text-h1-bold pb-32">
-          Hi, {userInfo?.user_metadata.first_name}
-        </h1>
-        <p className="pb-32">
-        Get started using Dance Break by clicking <span className="italic">Go to Timer</span> below. 
-        </p>
-        <section className="flex justify-center">
-          <Link to="/timer" className="primary-button flex justify-center" state={{ mode: "quiet" }}>
-            <button >Go to Timer</button>
-          </Link>
+        <div className="desktop:flex-1">
+          <img
+            className="w-full desktop:block hidden h-screen object-cover"
+            src="public/images/girl-dancing.jpg"
+            alt="Girl dancing in front of a yellow background"
+          ></img>
+        </div>
+        <section className="desktop:flex-1 px-64">
+          <LogoutButton />
+          {/* leaving logout here since we dont have menu yet*/}
+          <h1 className="font-dmSans text-h1-bold pb-32">
+            Hi, {userInfo?.user_metadata.first_name}
+          </h1>
+          <p className="pb-32">
+            Get started using Dance Break by clicking{" "}
+            <span className="italic">Go to Timer</span> below.
+          </p>
+          <section className="flex justify-center">
+            <Link
+              to="/timer"
+              className="primary-button flex justify-center"
+              state={{ mode: "quiet" }}
+            >
+              <button>Go to Timer</button>
+            </Link>
+          </section>
         </section>
-      </section>
       </>
     </PassageAuthGuard>
   );
